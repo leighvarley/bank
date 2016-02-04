f56929f1cc7c66de4154445f99fc0f879a62ab12
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-04 12:16:13 -0800

    JS for savings deposit and withdrawal

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ var checkingDeposit = function(){
 //Call the checking deposit function when the checking deposit button is clicked.
 document.getElementById("ckDepositButton").addEventListener("click", checkingDeposit);
 
-
-//Checking Withdrawl
+//Checking Withdrawal
 
 var checkingWithdrawal = function(){
   checkingBalance -= userCheckingInput();
@@ -30,4 +29,36 @@ var checkingWithdrawal = function(){
   ckBalanceContainer.innerHTML = "$" + checkingBalance;
 }
 
+//Call the checking withdrawal function when the checking withdrawal button is clicked.
 document.getElementById("ckWithdrawalButton").addEventListener("click", checkingWithdrawal);
+
+//SAVINGS ACCOUNT
+
+var savingsBalance = 0;
+
+//Grab user input for the savings account.
+var userSavingsInput = function(){
+  var svAmount = parseInt(document.getElementById("svInput").value);
+  document.getElementById("svInput").value = "";
+  return svAmount;
+}
+
+//Add new deposit to balance and save in the container.
+var savingsDeposit = function(){
+  savingsBalance += userSavingsInput();
+  var svBalanceContainer = document.getElementById("svBalanceContainer");
+  svBalanceContainer.innerHTML = "$" + savingsBalance;
+}
+
+//Call the savings deposit function when the savings deposit button is clicked
+document.getElementById("svDepositButton").addEventListener("click", savingsDeposit);
+
+//Subtract withdrawal from savings account and save balance in the container
+var savingsWithdrawal = function(){
+  savingsBalance -= userSavingsInput();
+  var svBalanceContainer = document.getElementById("svBalanceContainer");
+  svBalanceContainer.innerHTML = "$" + savingsBalance;
+}
+
+//Call the savings withdrawal function when the savings withdrawal button is clicked
+document.getElementById("svWithdrawalButton").addEventListener("click", savingsWithdrawal);
