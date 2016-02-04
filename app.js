fb931eeb1a1e277c68b99119ff931f95498de4a3
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-04 09:25:47 -0800

    JS for checking deposit

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-//add event listener to checking deposit button
-document.getElementById('ckDepositButton').addEventListener('click', function(){
-  console.log("working")
-})
+//CHECKING ACCOUNT
+
+var checkingBalance = 0;
+
+//Grab user input for the checking account
+var userCheckingInput = function() {
+  var ckAmount = parseInt(document.getElementById("ckInput").value);
+  return ckAmount;
+}
+
+//CHECKING DEPOSIT
+
+//Add new deposit to current balance and display it in the container.
+var checkingDeposit = function(){
+  checkingBalance += userCheckingInput();
+  var ckBalanceContainer = document.getElementById("ckBalanceContainer");
+  ckBalanceContainer.innerHTML = "$" + checkingBalance;
+ }
+
+//Call the checking deposit function when the checking deposit button is clicked.
+document.getElementById("ckDepositButton").addEventListener("click", checkingDeposit)
