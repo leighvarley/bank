e6b66f8b46c7d79831610bb03a05c0c1c1fc4b34
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-08 00:51:38 -0800

    deleted comments and console.log statements

--- app.js
+++ app.js
@@ -3,31 +3,20 @@ $(document).ready(function(){
 
   //DEPOSIT FUNCTION
   function makeDeposit(){
-    //Grab the targeted balance div.
     var currentBalance = $(this).siblings("div.balance");
-    console.log(currentBalance);
-    //Get the current balance from the div and save it as a string.
     var currentBalanceText = currentBalance.text();
-    console.log("currentBalanceText: " + currentBalanceText + " type: " + typeof(currentBalanceText));
-    //Convert the string to an integer.
     var balance = parseInt(currentBalanceText.replace("$", ""));
-    console.log("balance: " + balance + " type: " + typeof(balance));
-    //Grab the userInput and save it as an integer.
     var userInput = parseInt($(this).siblings("input.userInputAmount").val());
-    console.log("userInput:" + userInput);
-    //Calculate the new total and update the html to reflect the added userInput.
     $(currentBalance).html( function(){
         var total = balance + (userInput || 0);
         return "<h2>$" + total + "</h2>";
     });
-    //Clear the value of the input field.
     $(this).siblings("input.userInputAmount").val("");
   }
 
   //WITHDRAWAL FUNCTION
   function makeWithdrawal() {
     var currentBalance = $(this).siblings("div.balance");
-    console.log(currentBalance);
     var currentBalanceText = currentBalance.text();
     var balance = parseInt(currentBalanceText.replace("$", ""));
     var userInput = parseInt($(this).siblings("input.userInputAmount").val());
