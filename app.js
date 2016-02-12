ed8662a9648d3e6f983a2967b117ed7bef282244
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-11 18:10:34 -0800

    removed unnecessary else if statement and cleaned up spacing

--- app.js
+++ app.js
@@ -8,29 +8,23 @@ $(document).ready(function(){
     var userInput = parseInt($(this).siblings("input.userInput").val());
     var errorDiv = $(this).siblings("div.error");
 
-    if($(this).hasClass("deposit")){
+    if ($(this).hasClass("deposit")) {
       var total = startingBalance + (userInput || 0);
     } else {
-      if(startingBalance >= userInput){
+      if (startingBalance >= userInput) {
         var total = startingBalance - (userInput || 0);
-      } else if(startingBalance < userInput) {
-        total = startingBalance;
-        }
-        else {
+      } else {
           total = startingBalance;
         }
     }
 
-    if( ($(this).hasClass("withdrawal")) && (userInput > startingBalance) ) {
+    if (($(this).hasClass("withdrawal")) && (userInput > startingBalance)) {
       errorDiv.html("<p>Insufficient Funds</p>");
-      console.log("error");
-    }
-    else {
+    } else {
       errorDiv.html("");
-      console.log("ok");
     }
 
-    balanceDiv.html(function(){
+    balanceDiv.html(function() {
       return "<h2>$" + total + "</h2>";
     });
 
