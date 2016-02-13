6b10ac9e8aa57f57d512d3d29a4f9d490928db01
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-12 19:12:43 -0800

    moved code that updates the page into a method and cleaned up comments

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 
   var bank = {
+
     makeTransaction: function(){
+
       //Set up variables.
       var balanceDiv = $(this).siblings("div.balance");
       var balanceText = balanceDiv.text();
       var startingBalance = parseInt(balanceText.replace("$", ""));
       var userInput = parseInt($(this).siblings("input.userInput").val());
       var errorDiv = $(this).siblings("div.error");
-      //Update the total.
+      //Use self to access makeTransaction's "this" keyword from updateThePage.
+      var self = $(this);
+
+      //Calculate the total.
       if ($(this).hasClass("deposit")) {
         var total = startingBalance + (userInput || 0);
       } else {
@@ -17,22 +22,28 @@ $(document).ready(function(){
         } else {
             total = startingBalance;
           }
-      }
-      //Show the total on the page.
-      balanceDiv.html("<h2>$" + total + "</h2>");
-      //Show an error message if the requested withdrawal exceeds the balance.
-      if (($(this).hasClass("withdrawal")) && (userInput > startingBalance)) {
-        errorDiv.html("<p>Insufficient Funds!</p>");
-      } else {
-        errorDiv.html("");
-      }
-      //Clear the user input field.
-      $(this).siblings("input.userInput").val("");
+      };
+
+      //Display the total, show an error message if necessary, and clear the user input field.
+      var updateThePage = function(){
+        balanceDiv.html("<h2>$" + total + "</h2>");
+        if ((self.hasClass("withdrawal")) && (userInput > startingBalance)) {
+          errorDiv.html("<p>Insufficient Funds!</p>");
+        } else {
+          errorDiv.html("");
+        };
+        $("input.userInput").val("");
+      };
+
+      updateThePage();
+
     },
-    // Call the function when a button is clicked.
+
+    // Call makeTransaction when a button is clicked.
     listenForClick: function() {
       $("[type=button]").on("click", bank.makeTransaction)
     }
+
   }
 
   bank.listenForClick();
