e63c12a0dbb7d6bcfc902530b38c0cfc558a9241
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2016-02-12 17:52:23 -0800

    moved code into a bank object

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 $(document).ready(function(){
 
-  function makeTransaction(){
-
-    //Set up variables.
-    var balanceDiv = $(this).siblings("div.balance");
-    var balanceText = balanceDiv.text();
-    var startingBalance = parseInt(balanceText.replace("$", ""));
-    var userInput = parseInt($(this).siblings("input.userInput").val());
-    var errorDiv = $(this).siblings("div.error");
-
-    //Update the total.
-    if ($(this).hasClass("deposit")) {
-      var total = startingBalance + (userInput || 0);
-    } else {
-      if (startingBalance >= userInput) {
-        var total = startingBalance - (userInput || 0);
+  var bank = {
+    makeTransaction: function(){
+      //Set up variables.
+      var balanceDiv = $(this).siblings("div.balance");
+      var balanceText = balanceDiv.text();
+      var startingBalance = parseInt(balanceText.replace("$", ""));
+      var userInput = parseInt($(this).siblings("input.userInput").val());
+      var errorDiv = $(this).siblings("div.error");
+      //Update the total.
+      if ($(this).hasClass("deposit")) {
+        var total = startingBalance + (userInput || 0);
       } else {
-          total = startingBalance;
-        }
+        if (startingBalance >= userInput) {
+          var total = startingBalance - (userInput || 0);
+        } else {
+            total = startingBalance;
+          }
+      }
+      //Show the total on the page.
+      balanceDiv.html("<h2>$" + total + "</h2>");
+      //Show an error message if the requested withdrawal exceeds the balance.
+      if (($(this).hasClass("withdrawal")) && (userInput > startingBalance)) {
+        errorDiv.html("<p>Insufficient Funds!</p>");
+      } else {
+        errorDiv.html("");
+      }
+      //Clear the user input field.
+      $(this).siblings("input.userInput").val("");
+    },
+    // Call the function when a button is clicked.
+    listenForClick: function() {
+      $("[type=button]").on("click", bank.makeTransaction)
     }
-
-    //Show the total on the page.
-    balanceDiv.html("<h2>$" + total + "</h2>");
-
-    //Show an error message if the requested withdrawal exceeds the balance.
-    if (($(this).hasClass("withdrawal")) && (userInput > startingBalance)) {
-      errorDiv.html("<p>Insufficient Funds!</p>");
-    } else {
-      errorDiv.html("");
-    }
-
-    //Clear the user input field.
-    $(this).siblings("input.userInput").val("");
-
   }
 
-  //Call the function when a button is clicked.
-  $("[type=button]").on("click", makeTransaction);
+  bank.listenForClick();
 
 }());
